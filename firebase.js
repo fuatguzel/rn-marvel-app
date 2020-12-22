3ad3559cb5e5e2334d54ab80c9e319f5763b1d1a
Fuat Güzel <fuatguzeel@gmail.com>
2020-12-23 01:53:14 +0300

    Register ve firebase eklendi.

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBBBCtF0W4JHs3x0VhScB-OKv1k9EGngfE",
+    authDomain: "rn-expo-project.firebaseapp.com",
+    projectId: "rn-expo-project",
+    storageBucket: "rn-expo-project.appspot.com",
+    messagingSenderId: "851329721821",
+    appId: "1:851329721821:web:45bdc4430526b596d00234",
+    measurementId: "G-29KKDV04EQ"
+};
